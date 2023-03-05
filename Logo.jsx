e0d0cdd1514ec44deef528e2e25561b83851ebec
nachikethprk <126313865+nachikethprk@users.noreply.github.com>
2023-03-05 20:15:23 +0530

    Delete Logo.jsx

--- Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Logo=()=>{
-    return(
-     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt7cCI3su4crSg5RTvKqTCMZhGYzPAEsu2rA&usqp=CAU" alt="" />
-    )
-}
-export default Logo
